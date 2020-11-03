ecf86a529adcba376c16d09e200e09e648150851
Valentyn Ushych <valentinushych@gmail.com>
2020-11-03 02:50:38 +0200

    add implementation LongMap and Tests

--- src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
+++ src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
@@ -1,43 +1,197 @@
 package de.comparus.opensource.longmap;
 
+import java.util.*;
+
 public class LongMapImpl<V> implements LongMap<V> {
+    private static final double LOAD_FACTORY = 0.75;
+
+    private Entry<V>[] table;
+    private int capacity;
+    private int size;
+
+    public LongMapImpl() {
+        this.capacity = 16;
+        this.size = 0;
+        this.table = new Entry[capacity];
+    }
+
+    public LongMapImpl(long capacity) {
+        this.capacity = (int) capacity;
+        this.size = 0;
+        this.table = new Entry[(int) capacity];
+    }
+
     public V put(long key, V value) {
-        return null;
+        int hash = hash(key);
+        int index = indexFor(hash, capacity);
+        if (size > capacity * LOAD_FACTORY) {
+            resize();
+        }
+        if (key != 0) {
+            if (table[index] == null) {
+                addEntry(key, value, hash, index);
+            } else if (table[index].getHash() == hash && table[index].getKey() == key) {
+                table[index].setValue(value);
+            } else {
+                addNextEntry(key, value, hash, index);
+            }
+        } else {
+            putForZeroKey(key, value);
+        }
+        return value;
     }
 
     public V get(long key) {
-        return null;
+        int index = indexFor(hash(key), capacity);
+        if (table[index] != null) {
+            Entry<V> result = table[index];
+            return result.hasNext() ? iterateEntry(result, key).getValue() : result.getValue();
+        }
+        throw new IllegalArgumentException("Key not found");
     }
 
     public V remove(long key) {
-        return null;
+        if (containsKey(key)) {
+            V values = get(key);
+            int index = indexFor(hash(key), capacity);
+            table[index] = table[index].hasNext() ? iterateEntry(table[index], key) : null;
+            size--;
+            return values;
+        }
+        throw new IllegalArgumentException("Key not found");
     }
 
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     public boolean containsKey(long key) {
+        int index = indexFor(hash(key), capacity);
+        if (table[index] != null)
+            return iterateEntry(table[index], key).getKey() == key;
+
         return false;
     }
 
     public boolean containsValue(V value) {
-        return false;
+        Optional<Entry<V>> result = Arrays.stream(table)
+                .filter(Objects::nonNull)
+                .filter(entry -> entry.getValue() == value).findFirst();
+        return result.isPresent();
     }
 
     public long[] keys() {
-        return null;
+        long[] keys = Arrays.stream(table)
+                .filter(Objects::nonNull)
+                .mapToLong(Entry::getKey).toArray();
+        return keys;
     }
 
     public V[] values() {
-        return null;
+        V[] values = (V[]) Arrays.stream(table).filter(Objects::nonNull)
+                .map(Entry::getValue).toArray();
+        return values;
     }
 
     public long size() {
-        return 0;
+        return size;
     }
 
     public void clear() {
+        table = new Entry[capacity];
+        size = 0;
+    }
 
+    private Entry<V> iterateEntry(Entry<V> entry, long key) {
+        while (entry.hasNext()) {
+            if (entry.getKey() == key) {
+                entry = entry.next;
+            }
+        }
+        return entry;
+    }
+
+    private void resize() {
+        this.capacity *= 2;
+        Entry<V>[] newTable = new Entry[capacity];
+        transfer(newTable);
+        table = newTable;
+    }
+
+    private void transfer(Entry<V>[] newEntry) {
+        for (int i = 0; i < table.length; i++) {
+            if (table[i] != null) {
+                int index = indexFor(table[i].hash, capacity);
+                newEntry[index] = table[i];
+            }
+        }
+    }
+
+    private void addNextEntry(long key, V value, int hash, int index) {
+        this.table[index].setNext(new Entry<>(key, value, hash));
+        size++;
+    }
+
+    private void putForZeroKey(long key, V value) {
+        addEntry(key, value, 0, 0);
+    }
+
+    private void addEntry(long key, V value, int hash, int index) {
+        this.table[index] = new Entry<>(key, value, hash);
+        size++;
+    }
+
+    private int indexFor(int hash, int size) {
+        return hash & (size - 1);
+    }
+
+    private int hash(long key) {
+        return (int) (Long.hashCode(key) * 31 * LOAD_FACTORY);
+    }
+
+    public class Entry<V> implements Iterator<Entry<V>> {
+        private long key;
+        private V value;
+        private int hash;
+        private Entry<V> next;
+
+        public Entry(long key, V value, int hash) {
+            this.key = key;
+            this.value = value;
+            this.hash = hash;
+        }
+
+        public long getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        public int getHash() {
+            return hash;
+        }
+
+        public void setNext(Entry<V> next) {
+            this.next = next;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return next != null;
+        }
+
+        @Override
+        public Entry<V> next() {
+            if (next == null) {
+                throw new NoSuchElementException();
+            }
+            return next;
+        }
     }
 }
---
+++ src/test/java/de/comparus/opensource/longmap/LongMapImplTest.java
@@ -0,0 +1,82 @@
+package de.comparus.opensource.longmap;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class LongMapImplTest {
+    private LongMap longMap;
+
+    @Before
+    public void setUp() {
+        longMap = new LongMapImpl();
+    }
+
+    @Test
+    public void whenPutValues_shouldCorrectGetValues() {
+        longMap.put(1488L, "Spider");
+        assertEquals( "Spider", longMap.get(1488L));
+    }
+
+    @Test
+    public void whenCallKeys_shouldGetArrays() {
+        longMap.put(1827398712L, "NZVI");
+        longMap.put(189273L, "comparus");
+        longMap.put(3788L, "deviro");
+        long[] longArray = {1827398712L, 189273L, 3788L};
+        assertEquals(longMap.keys().length, longArray.length);
+        for (long key : longArray) {
+            assertTrue(longMap.containsKey(key));
+        }
+    }
+
+    @Test
+    public void whenCallValues_shouldGetArrays() {
+        longMap.put(1827398712L, "NZVI");
+        longMap.put(189273L, "comparus");
+        longMap.put(3788L, "deviro");
+        String[] valueArray = {"NZVI", "comparus", "deviro"};
+        assertEquals(longMap.values().length, valueArray.length);
+        for (String value : valueArray) {
+            assertTrue(longMap.containsValue(value));
+        }
+    }
+
+    @Test
+    public void whenInputMore_shouldReturnCorrectSize() {
+        for (long i = 1; i < 18; i++) {
+            longMap.put(i, "lol" + i);
+        }
+        assertEquals( 17, longMap.size());
+    }
+
+    @Test
+    public void whenCallRemove_shouldDeleteEntryByKey() {
+        longMap.put(1827398712L, "NZVI");
+        longMap.remove(1827398712L);
+        assertTrue(!longMap.containsKey(1827398712L));
+    }
+
+    @Test
+    public void whenCallClear_shouldClearAllEntry() {
+        longMap.put(1827398712L, Integer.MAX_VALUE);
+        longMap.put(189273L, "comparus");
+        longMap.put(3788L, "deviro");
+        longMap.clear();
+        assertTrue(!longMap.containsKey(189273L));
+        assertEquals( 0, longMap.size());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenCallGetWithIncorrectKey_shouldGetException() {
+        longMap.get(421L);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenCallRemoveWithIncorrectKey_shouldGetException() {
+        longMap.get(421L);
+    }
+
+}
